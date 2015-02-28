dbd5d868d8dcc0c9116851d3b48c27d9efdb6ea5
Bucky Roberts <buckyroberts@gmail.com>
2015-02-27 20:37:33 -0500

    Python Beginner tutorials.

---
+++ Python/03_python.py
@@ -0,0 +1,28 @@
+>>> Bucky Roberts
+SyntaxError: invalid syntax
+>>> "Bucky Roberts"
+'Bucky Roberts'
+>>> 'Bucky Roberts is awesome!'
+'Bucky Roberts is awesome!'
+>>> 'I don't think shes 18'
+SyntaxError: invalid syntax
+>>> "I don't think shes 18"
+"I don't think shes 18"
+>>> 'She said, "What part of the cow is the meatloaf from?" '
+'She said, "What part of the cow is the meatloaf from?" '
+>>> 'I don\'t think shes 18'
+"I don't think shes 18"
+>>> print("hey now brown cow")
+hey now brown cow
+>>> print('C:\Bucky\Desktop\nudePics')
+C:\Bucky\Desktop
+udePics
+>>> print(r'C:\Bucky\Desktop\nudePics')
+C:\Bucky\Desktop\nudePics
+>>> firstName = "Bucky "
+>>> firstName + "Roberts"
+'Bucky Roberts'
+>>> firstName + "McLovin"
+'Bucky McLovin'
+>>> firstName * 5
+'Bucky Bucky Bucky Bucky Bucky '
---
+++ Python/04_python.py
@@ -0,0 +1,23 @@
+>>> user = "Tuna McFish"
+>>> user[0]
+'T'
+>>> user[5]
+'M'
+>>> user[-1]
+'h'
+>>> user[-3]
+'i'
+>>> user[2:7]
+'na Mc'
+>>> user[:7]
+'Tuna Mc'
+>>> user[2:]
+'na McFish'
+>>> user[:]
+'Tuna McFish'
+>>> print('dsadasd')
+dsadasd
+>>> len('dfjhf22893hfsdfjkasdf')
+21
+>>> len(user)
+11
