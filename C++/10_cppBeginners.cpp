14b437b59be8755a793f1c1092f60716d738d650
NehuenD <NehuenD@users.noreply.github.com>
2015-03-02 06:27:39 -0800

    Added File

---
+++ C++/10_cppBeginners.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// This function takes a parameter.
+void printCrap(int x){
+    cout << "buckys favorite numbner is " << x << endl;
+}
+
+int main()
+{
+    printCrap(854); // Call the function.
+
+    return 0;
+}
+
