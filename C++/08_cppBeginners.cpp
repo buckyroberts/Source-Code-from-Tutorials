9d20362d5e33b2a3e59516b1b52177b8268181f1
NehuenD <NehuenD@users.noreply.github.com>
2015-03-02 06:08:33 -0800

    Added File

---
+++ C++/08_cppBeginners.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 98;
+    int b = 76;
+    
+    if(a > b){ // Tests if a is greater than b. 
+        cout << "Bucky is AWESOME!!!!" << endl;
+    }
+   
+    return 0;
+}
