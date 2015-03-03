e7d6d3384098dd1e540c4e496b644bde4d4c7bc8
Dronca Raul <raul.dronca@yahoo.com>
2015-03-03 15:29:42 +0200

    Functions with Multiple Parameters

---
+++ JavaScript/08_Functions(3).js
@@ -0,0 +1,7 @@
+function apples(one, two) {
+	document.write(one + " is better than " + two + "<br/>");
+}
+
+apples("Bucky", "Hobart");
+apples("Counting Crows", "Bieber");
+apples("strawberry", "coconut");
