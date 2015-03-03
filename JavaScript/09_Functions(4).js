a8592239eb9eb56de3e273dcca76ce8d343243a7
Dronca Raul <raul.dronca@yahoo.com>
2015-03-03 15:35:27 +0200

    The return Statement

---
+++ JavaScript/09_Functions(4).js
@@ -0,0 +1,14 @@
+// First Example
+function tooEasy() {
+	return "Gametime";
+}
+
+document.write(tooEasy());
+
+// Second Example
+function addNumbers(a, b) {
+	var c = a + b;
+	return c;
+}
+
+document.write(addNumbers(3,6));
