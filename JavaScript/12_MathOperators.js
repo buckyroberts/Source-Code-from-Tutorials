eabfd71a6e23be220be5855c84f668d1a25530ae
Dronca Raul <raul.dronca@yahoo.com>
2015-03-05 23:15:48 +0200

    Math Operators

---
+++ JavaScript/12_MathOperators.js
@@ -0,0 +1,26 @@
+/* 
+
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus
+++	Increment
+--	Decrement
+
+*/
+
+var apples = 4 * 4;
+document.write(apples);
+
+// Methods to increment
+// First Example
+var chop = 7;
+chop = chop + 1;
+document.write(chop);
+
+// Second Example
+chop++;
+
+// Decrement
+chop--;
