4c502970efa3a2ee144f4144bb9ff8f1e4f23373
Sebastian Ramadan <plebbyastian@gmail.com>
2015-03-06 23:21:32 +1030

    Update 05_cppBeginners.cpp
    Fix for #5 and #6

--- C++/05_cppBeginners.cpp
+++ C++/05_cppBeginners.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 
 using namespace std;
@@ -10,12 +11,24 @@ int main()
     
     cout << "Enter a number hoss! \n";
     cin >> a;
+    if (!cin.good()) {
+        cout << "That wasn't a number, wanker! \n";
+        return 0;
+    }
     
     cout << "Enter another number \n";
     cin >> b;
+    if (!cin.good()) {
+        cout << "That wasn't another number \n";
+        return 0;
+    }
     
-    sum = a+b;
-    cout << "the sum of those numbers is " << sum << endl;
-    
+    if ((a < 0 && b < 0 && INT_MIN - a > b) || (a > 0 && b > 0 && INT_MAX - a < b)) {
+        cout << "the sum of those numbers would cause an error" << endl;
+    }
+    else {
+        sum = a+b;
+        cout << "the sum of those numbers is " << sum << endl;
+    }
     return 0;
 }
