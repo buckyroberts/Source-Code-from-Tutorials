48bda0198e648b2a65e9e1f324469bd9fa431445
Nakib Momin <nbmomin14@gmail.com>
2015-03-07 00:36:39 -0500

    Added few more files

---
+++ C_Begineer/12.CBegineer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	
+	//Math operators
+	// + Addition 
+	// - Subtraction
+	// / Divide
+	// * Multiplication
+	// % Modulos or remainder
+	
+	int a = 86;
+	int b = 21;
+	
+	//Integer result (INT)
+	printf("%d \n", a/b);
+	
+	float c = 86.0;
+	float d = 21.0;
+		
+	//float resukt (FLOAT)
+	printf("%f \n", c/d);
+		
+	return 0;
+}
