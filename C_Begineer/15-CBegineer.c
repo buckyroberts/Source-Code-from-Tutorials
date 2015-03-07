781d5e317a4c894112258db4fc40722e5ee8f925
Nakib Momin <nbmomin14@gmail.com>
2015-03-07 01:05:01 -0500

    added few more

---
+++ C_Begineer/15-CBegineer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int pageViews = 0;
+	
+	pageViews = pageViews + 1;
+	printf("Pageviews: %d \n", pageViews);
+	
+	pageViews = pageViews + 1;
+	printf("Pageviews: %d \n", pageViews);
+		
+	pageViews = pageViews + 1;
+	printf("Pageviews: %d \n", pageViews);
+	
+	
+	float balance = 1000.00;
+	
+	balance *= 1.1;
+	printf("Balance: %f \n", balance);
+	
+	balance *= 1.1;
+	printf("Balance: %f \n", balance);
+	
+	balance *= 1.1;
+	printf("Balance: %f \n", balance);
+			
+}
+
