e43db6d72fdf97728efae3108cf60e9a1a12cf4c
Bucky Roberts <buckyroberts@gmail.com>
2015-03-31 18:26:43 -0400

    Added NodeJs files.

---
+++ Node.js/009_NodeJs/movies.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    printAvatar: function(){
+        console.log("Avatar");
+    },
+
+    printChappie: function(){
+        console.log("Chappie");
+    },
+
+    favMovie: "The Matrix"
+
+};
