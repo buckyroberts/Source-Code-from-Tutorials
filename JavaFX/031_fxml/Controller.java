eb5678bf898280a9cdb0c7f90c2d8fa6e4d50100
Bucky Roberts <buckyroberts@gmail.com>
2015-04-29 05:57:52 -0400

    FXML

---
+++ JavaFX/031_fxml/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+}
