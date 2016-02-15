93bfe3cfd77eaaddcfe8957c773793bad87a103b
sfolje0 <sfojcad@gmail.com>
2016-02-16 00:04:10 +0100

    corrected max_page into max_pages

--- Python/26_python.py
+++ Python/26_python.py
@@ -4,7 +4,7 @@ from b4 import BeautifulSoup
 
 def trade_spider(max_pages):
   page = 1
-  while page <= max_page:
+  while page <= max_pages:
     url = 'https://buckysroom.org/trade/search.php?page=' + str(page)
     source_code = requests.get(url)
     plain_text = source_code.text
