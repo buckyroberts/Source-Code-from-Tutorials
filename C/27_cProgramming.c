276b8daffdc6e1817263450a3972dd00d78b5d40
David Ross <sw1ayfe@gmail.com>
2016-05-09 10:59:58 +0100

    Adding tutorial 27 plus alt code

---
+++ C/27_cProgramming.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main()
+{
+	int bacon;
+
+	for(bacon; bacon <=10; bacon++) {
+	// for(bacon; bacon <=100; bacon+=8) {
+
+	    printf("Bacon is %d \n", bacon);
+	}
+	
+	return 0;
+}
