f0ed96a268bb735f91aeb74826c2487b5cb0568b
Panayiotis Managafas <mangafasxone@hotmail.com>
2016-07-12 02:25:26 +0300

    Delete 01_tkinter.py

--- 01_tkinter.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-root = Tk()  # creates a blank window
-theLabel = Label(root, text="This is too easy")
-theLabel.pack()  # basically places this widget inside the window
-root.mainloop()  # keeps the window open, the close button breaks the loop
