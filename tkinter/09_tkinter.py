9703d6d66c0fcd9d5a87658720a3d39f95ed217e
Panayiotis Managafas <mangafasxone@hotmail.com>
2016-07-17 03:55:30 +0300

    Update 09_tkinter.py

--- tkinter/09_tkinter.py
+++ tkinter/09_tkinter.py
@@ -15,4 +15,8 @@ subMenu.add_command(label="New...", command=doNothing)
 subMenu.add_separator()
 subMenu.add_command(label="Exit", command=doNothing)
 
+editMenu = Menu(menu)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Redo", command=doNothing)
+
 root.mainloop()
