fa7cfac20e8db2d4393810db02818ca5c25cce01
mohitisrani <mohitisrani@hotmail.com>
2017-05-11 14:35:03 -0400

    Added round parenthesis to getActionCommand
    edited event.getActionCommand; to event.getActionCommand()

--- Java_Beginners/52_53_54_javaBeginners.java
+++ Java_Beginners/52_53_54_javaBeginners.java
@@ -44,11 +44,11 @@ public class tuna extends JFrame{
 				String string = "";
 				
 				if(event.getSource()==item1)
-						string=String.format("field 1: %s", event.getActionCommand);
+						string=String.format("field 1: %s", event.getActionCommand());
 				else if(event.getSource()==item2)
-						string=String.format("field 2: %s", event.getActionCommand);
+						string=String.format("field 2: %s", event.getActionCommand());
 					else if(event.getSource()==item3)
-						string=String.format("field 3: %s", event.getActionCommand);
+						string=String.format("field 3: %s", event.getActionCommand());
 					else if(event.getSource()==passwordField)
 						string=String.format("password field  is : %s",event.getActionCommand);
 			
