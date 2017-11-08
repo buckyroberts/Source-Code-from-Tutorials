ff9b0e3873e95457b56906bff69438848d44d069
Naman Kumar Garg <31842804+Naman-Garg-06@users.noreply.github.com>
2017-11-09 02:34:24 +0530

    Update 09_python.py

--- Python/09_python.py
+++ Python/09_python.py
@@ -1,17 +1,17 @@
 #from 0 to 10
-for x in range(10):
+for x in range(10):  #it will print from 0 to 9.
     print(x)
 
 print("")
 
 #from 5 to 12
-for x in range(5, 12):
+for x in range(5, 12):  #it will print from 5 to 11. It is one less than the final number mentioned.
     print(x)
 
 print("")
 
 #from 10 to 40 increment value 5
-for x in range(10, 40, 5):
+for x in range(10, 40, 5):  #it will print from 10 to 35.
     print(x)
 
 print("")
