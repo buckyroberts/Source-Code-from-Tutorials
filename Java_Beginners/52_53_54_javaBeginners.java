38eadce02e5ce3a13193a872fd653456c1f6185f
viprottam <31253193+viprottam@users.noreply.github.com>
2018-09-30 00:29:42 +0530

    Update 52_53_54_javaBeginners.java

--- Java_Beginners/52_53_54_javaBeginners.java
+++ Java_Beginners/52_53_54_javaBeginners.java
@@ -44,13 +44,13 @@ public class tuna extends JFrame{
 				String string = "";
 				
 				if(event.getSource()==item1)
-						string=String.format("field 1: %s", event.getActionCommand);
+						string=String.format("field 1: %s", event.getActionCommand());
 				else if(event.getSource()==item2)
-						string=String.format("field 2: %s", event.getActionCommand);
+						string=String.format("field 2: %s", event.getActionCommand());
 					else if(event.getSource()==item3)
-						string=String.format("field 3: %s", event.getActionCommand);
+						string=String.format("field 3: %s", event.getActionCommand());
 					else if(event.getSource()==passwordField)
-						string=String.format("password field  is : %s",event.getActionCommand);
+						string=String.format("password field  is : %s",event.getActionCommand());
 			
 			JOptionPane.showMessageDialog(null, string);
 			}
