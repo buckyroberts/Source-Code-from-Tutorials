209b8a4c4a0ec8effdb8cb390fbe6598a64b983f
HarleenWadhwa <harleensingh0001@gmail.com>
2018-10-05 07:15:47 -0700

    add this

--- C/14_cProgramming.c
+++ C/14_cProgramming.c
@@ -9,9 +9,12 @@ int main()
 {
 
     float age1, age2, age3, average;
-    age1 = age2 = 4.0;
 
-    printf("Enter your age\n");
+    printf("Enter your age of 1st person\n");
+    scanf("%f", &age1);
+    printf("Enter your age of 2nd person\n");
+    scanf("%f", &age2);
+    printf("Enter your age of 3rd person\n");
     scanf("%f", &age3);
 
     average = (age1 + age2 + age3) / 3;
