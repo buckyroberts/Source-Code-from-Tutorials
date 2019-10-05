75008e04de33fc032a7e0fb53af624301038444f
aclopax <56058949+aclopax@users.noreply.github.com>
2019-10-05 15:20:36 +1000

    Create 57_javaBeginners.java

---
+++ Java_Beginners/57_javaBeginners.java
@@ -0,0 +1,43 @@
+// apples.java file
+
+class apples{
+	public static void main(String[] args){
+
+		DogList DLO = new DogList();
+		Dog d = new Dog();
+		DLO.add(d);
+	}
+}
+
+//Dog.java file
+
+public class Dog extends Animal{
+
+}
+
+//Fish.java file
+
+public class Fish extends Animal{
+
+}
+
+//Animal.java file
+
+public class Animal{
+
+}
+
+//DogList.java file
+
+public class DogList{
+	private Dog[] thelist = new Dog[5];
+	private int i=0;
+
+	public void add(Dog d){
+		if(i<thelist.length){
+			thelist[i]=d;
+			System.out.println("Dog added at index "+i);
+			i++;
+		}
+	}
+}
